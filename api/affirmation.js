f13d3ab41adbafdfd348f52a4302b6ecc836cdc5
Donal Doherty <73297590+donaldoherty1980@users.noreply.github.com>
2024-04-06 11:26:36 +0100

    Describe your changes here

--- api/affirmation.js
+++ api/affirmation.js
@@ -1,48 +1,25 @@
-// affirmation.js (Placed in the /api directory to be deployed as a Vercel Serverless Function)
+import { MongoClient } from 'mongodb';
 
-const fetch = await import('node-fetch').then(module => module.default);
-const MongoClient = require('mongodb').MongoClient;  
-const assert = require('assert');
-
-module.exports = async (req, res) => {
-    // Define the MongoDB Data API URL
-    const baseUrl = process.env.MONGODB_API_URL;  
-    const apiKey = process.env.MONGODB_API_KEY;  
-    const dbName = process.env.MONGODB_DATABASE_NAME;
-
-    // Setup the data payload
-    const dataPayload = {
-        collection: 'affirmationsbyid',
-        database: 'affirmations',
-        dataSource: 'ZenCluster',
-        filter: {}, // Add a filter if necessary or keep it to fetch random documents
-        projection: { "_id": 0, "affirmation": 1 }
-    };
-
-    // Set up the headers
-    const headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*', // To handle CORS
-        'api-key': apiKey,
-    };
+export default async (req, res) => {
+    const client = new MongoClient(process.env.MONGO_URI);
 
     try {
-        const response = await fetch(baseUrl, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(dataPayload)
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
+        await client.connect();
+        const database = client.db('affirmations');
+        const affirmations = database.collection('affirmationsbyid');
+        const projection = { "_id": 0, "affirmations": 1 };
         
-        // Assuming the affirmation text is directly in the response's document property
-        res.status(200).json({ text: data.document.affirmation });
+        // Assuming you want to get a random document from the collection
+        const count = await affirmations.countDocuments();
+        const random = Math.floor(Math.random() * count);
+        const randomAffirmation = await affirmations.findOne({}, { projection }).skip(random);
+
+        res.status(200).json({ text: randomAffirmation.affirmations });
     } catch (error) {
         console.error('Error fetching affirmation:', error);
         res.status(500).json({ error: error.message });
+    } finally {
+        await client.close();
     }
 };
+
