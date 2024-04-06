f1c1a8a24138aae3e8debbc780dad33bd74e9c1e
Donal Doherty <73297590+donaldoherty1980@users.noreply.github.com>
2024-04-06 10:21:18 +0100

    Describe your changes here

--- api/affirmation.js
+++ api/affirmation.js
@@ -1,22 +1,26 @@
-// Since node-fetch is an ES module, we are using dynamic import here.
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+// affirmation.js (Placed in the /api directory to be deployed as a Vercel Serverless Function)
+
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 module.exports = async (req, res) => {
     // Define the MongoDB Data API URL
-    const baseUrl = 'https://eu-west-2.aws.data.mongodb-api.com/app/data-bydap/endpoint/data/v1/action/findOne';
-    
+    const baseUrl = process.env.MONGODB_API_URL;
+    const apiKey = process.env.MONGODB_API_KEY;
+
     // Setup the data payload
     const dataPayload = {
         collection: 'affirmationsbyid',
         database: 'affirmations',
         dataSource: 'ZenCluster',
+        filter: {}, // Add a filter if necessary or keep it to fetch random documents
         projection: { "_id": 0, "affirmation": 1 }
     };
-    
+
     // Set up the headers
     const headers = {
         'Content-Type': 'application/json',
-        'api-key': 'zTuU87EBcMMe6YlXLozEQGihqNvEoQc4Yar7zEXeDHPQ9vB9PGSiFb2ai4u1cFBP'
+        'Access-Control-Allow-Origin': '*', // To handle CORS
+        'api-key': apiKey,
     };
 
     try {
@@ -32,11 +36,10 @@ module.exports = async (req, res) => {
 
         const data = await response.json();
         
-        // Assuming the affirmation text is directly in the response's document property, modify as needed.
+        // Assuming the affirmation text is directly in the response's document property
         res.status(200).json({ text: data.document.affirmation });
-
     } catch (error) {
         console.error('Error fetching affirmation:', error);
-        res.status(500).json({ error: 'Error fetching affirmation' });
+        res.status(500).json({ error: error.message });
     }
 };
